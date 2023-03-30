83052f92a3903a9f3be8e253d3acceff16f53b18
Rafael Gonzaga <111755586+rgonzagaDTT@users.noreply.github.com>
2023-03-30 16:45:00 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Curso de Java");
+        curso1.setCargaHoraria(8);
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Javascript");
+        curso2.setDescricao("Curso de Javascript");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição da mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+    
+}
